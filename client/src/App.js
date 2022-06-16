9bedb0b939ffe1a13bc90798d520b4739e1816ba
TAHAWY <70866079+tahawy111@users.noreply.github.com>
2022-06-16 04:40:54 +0200

    insert react

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+export class App extends Component {
+  state = {};
+  render() {
+    return <div>App</div>;
+  }
+}
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -6,6 +6,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 const PORT = process.env.PORT || 3002;
 
+// models
+const FoodModel = require("./models/FoodModel");
+
+app.get("/", async (req, res) => {
+  const food = new FoodModel({ foodName: "Apple", daysSinceIAte: 3 });
+  try {
+    await food.save();
+    res.send("inserted Data");
+  } catch (error) {
+    console.log(err);
+  }
+});
+
 mongoose.connect(process.env.DB_URI, () => {
   try {
     console.log("DB Connected");
